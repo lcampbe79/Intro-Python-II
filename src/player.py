6cbdba76d19edf2d662f35c1220fe589d927f7fd
Lisa Campbell <lisa@Lisas-MBP.lan>
2020-02-12 16:20:56 -0800

    Prints Player and Room name

--- src/player.py
+++ src/player.py
@@ -1,11 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, room):
+    def __init__(self, name, current_room):
         self.name = name
-        self.room = room
+        self.current_room = current_room
     def __str__(self):
-        return f"Player: {self.name}, is currently in {self.room}"
+        return f"Player: {self.name} \n{self.current_room}"
 
 
 # player = Player("Lisa", "outside")
--- src/room.py
+++ src/room.py
@@ -1,6 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, desc):
-        name.self = name
-        desc.name = desc
+    def __init__(self, name, desc, n_to=None, s_to=None, e_to=None, w_to=None):
+        self.name = name
+        self.desc = desc
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+    def __str__(self):
+        return f"Room: {self.name}"
